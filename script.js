110ac1cc692f8d47e3734406741c7d094979a863
foxkodland <foxkodland@gmail.com>
2024-02-22 11:57:21 +0300

    circle перенёс в индикатор

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ list_li.forEach((li) => {
 
         // подвинуть индикатор к нажатой кнопке
         let number = li.dataset.number;
-        indicator.style.transform = 'translateX(' + (number - 1) * (70 + 20) + "px";
+        indicator.style.transform = 'translateX(' + (number - 1) * (70 + 20) + "px";  // 70 + 20 - это ширина nav item + gap flex
+
+        // удалить класс и почти сразу вернуть - чтобы у circle сработала заново анимация увеличения
+        indicator.classList.remove('active');
+        setTimeout(function(){indicator.classList.add('active')}, 10);
     })
 })
